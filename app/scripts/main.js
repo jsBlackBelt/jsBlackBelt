502412a2f8b9cd246f2b415d2596e84f3b3dbeaf
jsblackbelt <sefi.ninio@gmail.com>
2014-05-13 11:41:29 +0300

    TimingSrv

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('jsBlackBeltApp')
+angular.module('jsBlackBelt')
     .controller('MainCtrl', function ($scope) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
